4818ecaacad575fac109cfc90fdd606fef836719
Agbeniga Jesulayomi Timothy <jesulayomize55@gmail.com>
2023-04-04 12:52:15 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
-
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
 function App() {
-  return <h1>Hello World</h1>;
+  const [people, setPeople] = useState(data)
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])} className='btn'>Clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
